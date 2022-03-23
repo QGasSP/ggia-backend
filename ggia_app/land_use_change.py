b72758305ed7f600050c6102a34c882a02000e23
Mir Jalal Hashimli <mircelal99@gmail.com>
2022-03-23 20:33:22 +0200

    fixed bugs

--- ggia_app/land_use_change.py
+++ ggia_app/land_use_change.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Blueprint
 from flask import request
 from marshmallow import ValidationError
@@ -5,7 +7,6 @@ from ggia_app.transport_schemas import *
 from ggia_app.models import *
 from ggia_app.env import *
 import humps
-
 
 blue_print = Blueprint("land-use-change", __name__, url_prefix="/api/v1/calculate/land-use-change")
 
@@ -19,23 +20,26 @@ def calculate_land_use_change(country, land_use_change_dict, affected_years, cur
     if country_data is None:
         country_data = Country.query.filter_by(dataset_name=country).first()
     result_dict = {}
-    
+
     for land_type in LAND_TYPES_LIST:
         result = 0
         for key in LAND_USE_CHANGE_FACTOR_NAMES.keys():
             if affected_years.get(land_type, current_year) > current_year:
                 continue
-            if land_type in ZERO_LIST_FIRST_20_LAND_TYPES and key in ZERO_LIST_FIRST_20_LAND_USE_CHANGE_FACTOR_NAMES and current_year != affected_years.get(land_type, current_year):
+            if land_type in ZERO_LIST_FIRST_20_LAND_TYPES and key in ZERO_LIST_FIRST_20_LAND_USE_CHANGE_FACTOR_NAMES and current_year != affected_years.get(
+                    land_type, current_year):
                 continue
-            value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=land_type, factor_name=key).first().factor_value
+            value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=land_type,
+                                                  factor_name=key).first().factor_value
             result += \
                 land_use_change_dict[LAND_USE_CHANGE_FACTOR_NAMES[key]][land_type] \
                 * LAND_USE_CHANGE_CONVERSION_FACTOR \
                 * value
 
-        result_dict[land_type] = result  
+        result_dict[land_type] = result
 
     return result_dict
+
 
 def find_land_type_remaining(key):
     for land_type in YEARS_21_TO_30_DICT.keys():
@@ -60,7 +64,7 @@ def calculate_land_use_change_21_to_30(country, land_use_change_dict, affected_y
                 continue
             try:
                 value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=remaining,
-                                                  factor_name=key).first().factor_value
+                                                      factor_name=key).first().factor_value
                 result += \
                     land_use_change_dict[LAND_USE_CHANGE_FACTOR_NAMES[key]].get(land_type, 1) \
                     * LAND_USE_CHANGE_CONVERSION_FACTOR \
@@ -70,6 +74,41 @@ def calculate_land_use_change_21_to_30(country, land_use_change_dict, affected_y
 
         result_dict[land_type] = result
 
+    return result_dict
+
+
+def calculate_land_use_change_new(country, land_use_change_dict, policy_start_years, current_year):
+    country_data = Country.query.filter_by(name=country).first()
+    if country_data is None:
+        country_data = Country.query.filter_by(dataset_name=country).first()
+    result_dict = {}
+
+    for land_type in LAND_TYPES_LIST:
+        result = 0
+        remaining = find_land_type_remaining(land_type)
+        for key in LAND_USE_CHANGE_FACTOR_NAMES.keys():
+            if current_year < policy_start_years[land_type]:
+                continue
+            if current_year == policy_start_years[land_type]:
+                value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=land_type,
+                                                      factor_name=key).first().factor_value
+            else:
+                if land_type in ZERO_LIST_FIRST_20_LAND_TYPES and \
+                        key in ZERO_LIST_FIRST_20_LAND_USE_CHANGE_FACTOR_NAMES and \
+                        current_year < policy_start_years[land_type] + 20:
+                    continue
+                elif current_year >= policy_start_years[land_type] + 20:
+                    value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=remaining,
+                                                          factor_name=key).first().factor_value
+                else:
+                    value = LandUseChange.query.filter_by(country_id=country_data.id, land_conversion=land_type,
+                                                          factor_name=key).first().factor_value
+
+            result += \
+                land_use_change_dict[LAND_USE_CHANGE_FACTOR_NAMES[key]].get(land_type, 1) \
+                * LAND_USE_CHANGE_CONVERSION_FACTOR \
+                * value
+        result_dict[land_type] = result
     return result_dict
 
 
@@ -84,13 +123,12 @@ def route_land_use_change():
     land_use_change_dict = request_body["land_use_change"]
 
     land_use_change_response = dict()
-
-    for year in range(start_year, start_year + 21):
-        land_use_change_response[year] = calculate_land_use_change(country, land_use_change_dict, policy_start_years, year, start_year)
-
-    for year in range(start_year + 21, 2051):
-        land_use_change_response[year] = calculate_land_use_change_21_to_30(country, land_use_change_dict, policy_start_years, year)
-          
+    start = datetime.datetime.now()
+    for year in range(start_year, 2051):
+        land_use_change_response[year] = calculate_land_use_change_new(country, land_use_change_dict,
+                                                                       policy_start_years, year)
+    finish = datetime.datetime.now()
+    print(finish-start)
     return humps.camelize({
         "status": "success",
         "data": {
